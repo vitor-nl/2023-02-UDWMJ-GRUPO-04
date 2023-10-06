d800dec92b498f271fb6da00d1d999ef4a261e34
Natan Oliveira da Silva - 12922114821 <12922114821@ulife.com.br>
2023-10-05 21:22:14 -0300

    add corrigido client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+class Client():
+    
+    def __init__(self, first_name, last_name, addres, cell_phone, email, gender):
+        self.first_namefirst_name = first_name
+        self.last_name = last_name
+        self.addres = addres
+        self.cell_phone = cell_phone
+        self.email = email
+        self.gender = gender
