84d6d2d9cdda012c5a76b13033b242d7506c14aa
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2023-10-09 21:33:37 -0300

    Ajustes

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from order import Order
 from orderitem import Orderitem
 
 #Cliente
-rogerim = Client("Rogerim", "python", "Rua thurusbango thurusbago", "(51) 99999999", "Rogerim@gmail.com", "M")
+rogerim = Client("Rogerim", "python", "Rua thurusbango thurusbago",
+                 "(51) 99999999", "Rogerim@gmail.com", "M")
 print(f"\n{rogerim.first_name} {rogerim.last_name}, se cadastrou na loja.\n")
 
 #Categorias dos produtos
@@ -13,17 +14,20 @@ tenis = Category("0001", "Tenis", "Categorias de tenis")
 print(f"Categiria {tenis.name} foi cadastrada!\n")
 
 #Produtos
-nike_sb = Product("Tenis Nike SB", "Tenis nike sb, muito bom!", "01/01/1980", 1, tenis) #1 = Ativo, #2 Inativo
+nike_sb = Product("Tenis Nike SB", "Tenis nike sb, muito bom!", "01/01/1980",
+                  1, tenis)  #1 = Ativo, #2 Inativo
 print(f"Produto {nike_sb.name} foi cadastrado!\n")
 
 #Ordem de compra
-Rogerim_que_compra = Order(1, rogerim)#1 = Ativo, #2 Inativo
-print(f"Cliente: {Rogerim_que_compra.client.first_name}, realizou uma ordem de compra de um produto produto no valor de {Rogerim_que_compra.total_price}\n")
+Rogerim_que_compra = Order(1, rogerim)  #1 = Ativo, #2 Inativo
+print(
+    f"Cliente: {Rogerim_que_compra.client.first_name}, realizou uma ordem de compra de um produto produto no valor de {Rogerim_que_compra.total_price}\n"
+)
 
 #Comprando item
 Rogerim_ta_comprando = Orderitem(2, 200, Rogerim_que_compra, tenis)
-print(f"Rogerim ta comprando o produto: {Rogerim_ta_comprando.product.name}")
+print(f"Rogerim ta comprando o produto: {Rogerim_ta_comprando.product.name}\n")
 
-Rogerim_que_compra.total_price(Rogerim_ta_comprando)
+Rogerim_que_compra.add_total_price(Rogerim_ta_comprando)
 
-print(Rogerim_que_compra.total_price)
+print(f"O valor total da compar deu: R${Rogerim_que_compra.total_price}")
--- order.py
+++ order.py
@@ -1,14 +1,12 @@
-from itertools import product
-from typing import Type
-
-from typing import TypeVar
 from orderitem import Orderitem
 
-class Order():
-    def __init__(self, total_price, status, client):
-        self.total_price = None
-        self.status = status
-        self.client = client
 
-    def total_price(self, total_price: Orderitem):
-        self.toal_price += total_price.unitary_price
+class Order():
+
+  def __init__(self, status, client):
+    self.total_price = 0
+    self.status = status
+    self.client = client
+
+  def add_total_price(self, total_price: Orderitem):
+    self.total_price += total_price.unitary_price * total_price.quantity
