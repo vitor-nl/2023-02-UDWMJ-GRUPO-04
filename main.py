906409165a53db12340d24bf246944565cc2ab6d
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2023-10-09 21:20:24 -0300

    add funcao order

--- main.py
+++ main.py
@@ -17,9 +17,13 @@ nike_sb = Product("Tenis Nike SB", "Tenis nike sb, muito bom!", "01/01/1980", 1,
 print(f"Produto {nike_sb.name} foi cadastrado!\n")
 
 #Ordem de compra
-Rogerim_que_compra = Order(400.00, 1, rogerim)#1 = Ativo, #2 Inativo
+Rogerim_que_compra = Order(1, rogerim)#1 = Ativo, #2 Inativo
 print(f"Cliente: {Rogerim_que_compra.client.first_name}, realizou uma ordem de compra de um produto produto no valor de {Rogerim_que_compra.total_price}\n")
 
 #Comprando item
 Rogerim_ta_comprando = Orderitem(2, 200, Rogerim_que_compra, tenis)
 print(f"Rogerim ta comprando o produto: {Rogerim_ta_comprando.product.name}")
+
+Rogerim_que_compra.total_price(Rogerim_ta_comprando)
+
+print(Rogerim_que_compra.total_price)
--- order.py
+++ order.py
@@ -1,5 +1,14 @@
+from itertools import product
+from typing import Type
+
+from typing import TypeVar
+from orderitem import Orderitem
+
 class Order():
     def __init__(self, total_price, status, client):
-        self.total_price = total_price
+        self.total_price = None
         self.status = status
         self.client = client
+
+    def total_price(self, total_price: Orderitem):
+        self.toal_price += total_price.unitary_price
